91bdc9b0b2d0e0ff28bbcd88b50f568519d6ae34
Blake Sager <bsager@atlassian.com>
2022-06-01 16:27:40 -0500

    add dangling OO III classes

---
+++ cs-1331-intro-to-java/III/Scratch.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Scratch {
+
+  public static int f(int x, int y) {
+    if (x == 0) {
+        return y;
+    } else {
+        return f(x - 1, y + 2);
+    }
+}
+  public static void main(String[] args) {
+    System.out.println(f(3,4));
+  }
+}
